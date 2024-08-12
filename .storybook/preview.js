e015209435ce62e63f8bcaff4695e0ecaa55b9a1
fmoyano90 <f.moyano90@gmail.com>
2024-08-11 22:23:45 -0400

    Storybook installed and configured

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import '../src/assets/sass/Style.css'
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
